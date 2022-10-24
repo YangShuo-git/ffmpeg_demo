fe2278f29bc58b7ce759d84eac297591a0a690d9
yangshuo <17801003317@163.com>
2022-10-25 01:23:50 +0800

    解码视频流

--- demo_decode/main.cpp
+++ demo_decode/main.cpp
@@ -6,36 +6,38 @@ int main(int argc, const char * argv[]) {
     std::string curFile(__FILE__);
     cout << curFile << "\n";
 
-    #if 0
-    unsigned long pos = curFile.find("demo-mac");
-    if (pos == string::npos) {
+    #if 1
+    unsigned long pos = curFile.find("demo_decode");
+    if (pos == std::string::npos) {
         return -1;
     }
 
-    string resourceDir = curFile.substr(0, pos)+"filesources/";
+    std::string resourceDir = curFile.substr(0, pos)+"assets/";
+    cout << resourceDir << "\n";
     
     int test_use = 3;
-    if (test_use == 7) {
-        string srcPath = resourceDir + "test_1280x720_3.mp4";
-        SoftDecoder decoder;
+    SoftDecoder decoder;
+
+    if (test_use == 3) {
+        std::string srcPath = resourceDir + "2_audio.mp4";
         decoder.decodeFormat(srcPath);
     } else if (test_use == 8) {
-        string srcPath = resourceDir + "test_441_f32le_2.aac";
-        SoftDecoder decoder;
+        std::string srcPath = resourceDir + "test_441_f32le_2.aac";
+        // SoftDecoder decoder;
         decoder.decodeStream(srcPath);
     } else if (test_use == 9) {
-        string srcPath = resourceDir + "test_640x360_yuv420p.yuv";
-        string dstPath = resourceDir+ "1-abc-test.h264";
-        SoftDecoder decoder;
+        std::string srcPath = resourceDir + "test_640x360_yuv420p.yuv";
+        std::string dstPath = resourceDir+ "1-abc-test.h264";
+        // SoftDecoder decoder;
         decoder.encodeAsStream(srcPath,dstPath);
     }
     // } else if (test_use == 10) {
-    //     string srcPath = resourceDir + "test_1280x720_3.mp4";
+    //     std::string srcPath = resourceDir + "test_1280x720_3.mp4";
     //     HardEnDecoder decoder;
     //     decoder.decodeFormat(srcPath);
     // } else if (test_use == 11) {
-    //     string srcPath = resourceDir + "test_640x360_yuv420p.yuv";
-    //     string dstPath = resourceDir + "3-test.h264";
+    //     std::string srcPath = resourceDir + "test_640x360_yuv420p.yuv";
+    //     std::string dstPath = resourceDir + "3-test.h264";
     //     HardEnDecoder decoder;
     //     decoder.doEncode(srcPath,dstPath);
 
